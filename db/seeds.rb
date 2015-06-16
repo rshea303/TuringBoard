d4c4058cc0897d50871d286db36d867a9bf2af07
Rich Shea <richcshea@gmail.com>
2015-06-16 10:50:51 -0600

    add tickets to seed filew

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,17 @@
-Board.create(title: "Dinner Dash")
-Board.create(title: "The Pivot")
-Board.create(title: "Mythical Creatures")
+def boards 
+  [Board.create(title: "Dinner Dash"),
+   Board.create(title: "The Pivot"),
+   Board.create(title: "Mythical Creatures")]
+end
+
+def tickets
+  boards.each do |board|
+    5.times do 
+      board.tickets.create(title: "new ticket #{rand(1..100)}")
+    end
+  end
+end
+
+tickets
+
+
